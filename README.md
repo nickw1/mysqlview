720c100a6dbe59d9534d58854d0ec5165e3ab476
Nick Whitelegg <nickw4426@gmail.com>
2023-01-07 19:44:44 +0000

    Add simple editing functionality

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # Node MySQL visualiser
 
-An extremely simple Node and Express-based visualiser for a MySQL database.
-Settings should be specified in `.env`:
+An extremely simple Node and Express-based visualiser and editor for a MySQL database.
+Settings should be specified in `.env` (an example is supplied):
 
 - `DB_HOST` - database host machine, usually `localhost`
-- `DB_USER` - database user
-- `DB_PASS` - database password
-- `DB_DBASE` - database name
+- `ID_COLUMN` - the column to use for the ID.
 - `TABLES` - the tables you want to visualise in JSON array format, e.g
 
 ```
 TABLES=["wadsongs","ht_users"]
 ```
 
-It is assumed that the tables contain an `id` column as they will be ordered by ID. If not, the server will fail!
+It is assumed that all tables use the same column name for the ID, specified with the `ID_COLUMN` setting. 
 
 
 ## Installing and running
--- app.mjs
+++ app.mjs
@@ -5,29 +5,96 @@ import 'dotenv/config';
 const app = express();
 app.set('view engine', 'ejs');
 
-const con = await mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DBASE
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+let con, tables = process.env.TABLES ? JSON.parse(process.env.TABLES): null;
+const idCol = process.env.ID_COLUMN || 'ID'
+
+
+
+app.post('/login', async(req, res) => {
+    try {
+        con = await mysql2.createConnection({
+            host: process.env.DB_HOST,
+            user: req.body.username,
+            password: req.body.password, 
+            database: req.body.database 
+        });
+        if(!tables) {
+            const [results, fields] = await con.execute(`SELECT * FROM information_schema.tables AS t WHERE table_schema='${req.body.database}'`);
+            tables = results.map(result => result.TABLE_NAME);
+        }
+        res.redirect('/');
+    } catch(e) {
+        console.error(e);
+        res.redirect('/login.html');
+    }
 });
 
-const tables = JSON.parse(process.env.TABLES);
+app.post('/logout', (req, res) => {
+    console.log('Ending');
+    con.destroy();
+    con = null;
+    res.redirect('/login.html');
+});
 
-/* How you can obtain all tables in the database
-const [results, fields] = await con.query(`SELECT * FROM information_schema.tables AS t WHERE table_schema='${process.env.DB_DBASE}'`);
-const tables = results.map(result => result.TABLE_NAME);
-*/
+app.use((req, res, next) => {
+    if(!con) {
+        res.redirect('/login.html');
+    } else {
+        next();
+    }
+});
 
 app.get('/', async(req, res) => {
-    
     const allResults = {};
+    req.query.error = req.query.error || "";
     for(let table of tables) {
-        const [results, fields] = await con.query(`SELECT * FROM ${table} ORDER BY id`);
-        const fieldNames = fields.map (fieldInfo => fieldInfo.name);
-        allResults[table] = { results: results, fieldNames: fieldNames };    
+        try {
+            const [results, fields] = await con.execute(`SELECT * FROM ${table} ORDER BY id`);
+            const fieldNames = fields.map (fieldInfo => fieldInfo.name);
+            allResults[table] = { results: results, fieldNames: fieldNames };    
+        } catch(e) {
+            req.query.error += `Table ${table}: Error: ${e}`;
+        }
     }
-    res.render('index', { allResults: allResults} );
+    res.render('index', { allResults: allResults, idCol: idCol, error: req.query.error } );
+});
+
+
+
+app.post('/:table([a-zA-Z_]+)/row/:id(\\d+)', async(req, res)=> {
+    let sql = `UPDATE ${req.params.table} SET `;
+    const placeholders  = Object.keys(req.body).map(col => `${col}=?`);
+    sql += placeholders.join(',') + ` WHERE ${idCol}=${req.params.id}`;
+    try {
+        const [results, fields] = await con.execute(sql, Object.values(req.body));
+        res.redirect('/');
+    } catch(e) {
+        res.redirect(`/?error=${e.message}, caused by "${e.sql}"`);
+    }
+});
+
+app.post('/:table([a-zA-Z_]+)/row/:id(\\d+)/delete', async(req, res) => {
+    try {
+        const [results, fields] = await con.execute(`DELETE FROM ${req.params.table} where ${idCol}=${req.params.id}`);
+        res.redirect('/');
+    } catch(e) {
+        res.redirect(`/?error=${e.message}, caused by "${e.sql}"`);
+    }
+});
+    
+app.post('/:table([a-zA-Z_]+)/row/create', async(req, res) => {
+    const cols = Object.keys(req.body);
+    let sql = `INSERT INTO ${req.params.table} (` + cols.join(',') + `) VALUES (` + cols.map(col => '?').join(',') + `)`;
+    try {
+        const [results, fields] = await con.execute(sql, Object.values(req.body));
+        res.redirect('/');
+    } catch(e) {
+        res.redirect(`/?error=${e.message}, caused by "${e.sql}"`);
+    }
 });
 
 app.listen(3100);
